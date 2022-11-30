13371761106fc285b0bcc216a14aa4122a1d3a1c
Bora Varol <varolbora5@gmail.com>
2022-11-30 11:35:26 +0100

    Fucking header done finally

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,14 @@
+import ari from "../assets/ARI logo.jpg"
+import gentiger from "../assets/GT logo.png"
+
 export function Header() {	
 	return (
 	<header>
-		<div className="flex">
-			<img src="../assets/ARI logo.jpg" alt="Ari Makina"/>
-			<h1 className="max-w-">ARI MAKİNA TAKIM TEZGAHLARI SANAYİ ve TİCARET LTD. ŞTİ.</h1>
-			<img src="../assets/GT logo.png" alt="Gentiger"/>
-		</div>
+			<div className="flex items-center justify-center text-center m-5">
+				<img src={ari} className="aspect-auto max-w-[10%]" alt="Ari Makina"/>
+				<h1>ARI MAKİNA TAKIM TEZGAHLARI SANAYİ ve TİCARET LTD. ŞTİ.</h1>
+				<img src={gentiger} className="aspect-auto" alt="Gentiger"/>
+			</div>
 	</header>
 	)
 }
